3b729506ff6d1674994a92e2f12771f6c2ffa1ec
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-11-02 19:50:45 +0700

    update logout

--- postfunc.js
+++ postfunc.js
@@ -3,7 +3,7 @@ import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignUp(){
-    let target_url = "https://asia-southeast2-rock-prism-401900.cloudfunctions.net/Login";
+    let target_url = "https://asia-southeast2-rock-prism-401900.cloudfunctions.net/gcpproyek3";
     let tokenkey = "token";
     let tokenvalue = "40e27523381e236813ece16a5e420192592d2ed80cd74eafe115afbaebe874d2"; 
     let datainjson = {
