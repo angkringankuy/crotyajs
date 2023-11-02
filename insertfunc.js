4ed229dfc8fcdb221f3c95ff23589a93eb44b963
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-11-02 19:30:13 +0700

    update logout

--- insertfunc.js
+++ insertfunc.js
@@ -1,32 +1,30 @@
-import { postWithToken } from "https://jscroot.github.io/api/croot.js";
-import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
-import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
+import { postWithBearer } from "https://jscroot.github.io/api/croot.js";
+export let URLPost = `https://asia-southeast2-rock-prism-401900.cloudfunctions.net/Register`
+export let token = 'token';
 
-export default function PostSignUp(){
-    let target_url = "https://asia-southeast2-rock-prism-401900.cloudfunctions.net/warmindo-insertuser";
-    let tokenkey = "token";
-    let tokenvalue = "40e27523381e236813ece16a5e420192592d2ed80cd74eafe115afbaebe874d2"; 
-    let datainjson = {
-        "username": getValue("username"),
-        "password": getValue("password"),
-        "role": getValue("role")
-    }
 
-    postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
 
-}
-function responseData(result) {
-    setInner("pesan", result.message);
-    setCookieWithExpireHour("token", result.token, 2);
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        let data = GetDataForm();
+        postWithBearer(URLPost, token, data, ResponsePost)
+    });
+});
+
+    export function GetDataForm(){
+        const username = document.querySelector("#username").value;
+        const password = document.querySelector("#password").value;
+        console.log(password)
     
-    if (result.message === "Selamat Datang") {
-        // Jika pesan adalah "Selamat Datang", arahkan ke halaman dashboard.
-        window.location.href = "dashboard.html"; // Gantilah "error.html" dengan halaman error yang sesuai.
-    } else if (result.message === "Password Salah") {
-        // Jika pesan kesalahan adalah "Password salah", arahkan ke halaman error.
-        window.location.href = "404.html";
-    } else {
-        // Penanganan lainnya (pesan kesalahan lainnya)
-        window.location.href = "index.html";
+        const data = {
+            username: username,
+            password: password,
+        };
+        return data
     }
-}
+    
+    export function ResponsePost(result) {
+        AlertPost(result);
+    }
